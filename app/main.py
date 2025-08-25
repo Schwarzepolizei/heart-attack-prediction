321f61b1f7cf568171e5f7994dc3ec4f8f31c061
Schwarzepolizei <schwarzepolizei@gmail.com>
2025-08-25 13:17:38 +0400

    Изменен main.py

--- app/main.py
+++ app/main.py
@@ -1,43 +1,27 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from .schemas import PatientData
+from .model import predict_single, predict_batch
 import pandas as pd
-import pickle
 
-# Загружаем модель
-with open("cat_model.pkl", "rb") as f:
-    model = pickle.load(f)
 
 app = FastAPI(title="Heart Attack Risk Prediction API")
 
-class PatientData(BaseModel):
-    bmi: float
-    sedentary_hours_per_day: float
-    cholesterol: float
-    triglycerides: float
-    systolic_blood_pressure: float
-    diastolic_blood_pressure: float
-    exercise_hours_per_week: float
-    income: float
-    heart_rate: float
-    age: float
-    diet: int
-    physical_activity_days_per_week: int
-    sleep_hours_per_day: float
-    stress_level: int
-    blood_sugar: float
-    obesity: int
-    alcohol_consumption: int
-    family_history: int
-    previous_heart_problems: int
-    diabetes: int
-    medication_use: int
-    gender: int
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def main_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload-csv", response_class=HTMLResponse)
+async def upload_csv(request: Request, file: UploadFile = File(...)):
+    df = pd.read_csv(file.file)
+    predictions = predict_batch(df)
+    return templates.TemplateResponse("results.html", {"request": request, "predictions": predictions})
 
 @app.post("/predict")
 def predict_risk(patient: PatientData):
-    input_df = pd.DataFrame([patient.dict()])
-    
-   
-    prediction = model.predict(input_df)[0]
-    
-    return {"prediction": int(prediction)}
+    prediction = predict_single(patient.dict())
+    return {"prediction": prediction}
